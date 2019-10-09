611df000a2d3c5837b1af2ba744411cae9bd61b3
Eugenio <hedgehog0996@gmail.com>
2019-10-10 00:25:15 +0300

    function to get path of public/private keys

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import tkinter
+from tkinter import filedialog
+from tkinter import messagebox
 
 
 class KeyInputBlock:
+    file_path = ''
 
-    def __init__(self, tk, label_text):
+    def __init__(self, tk, label_text, file_format):
         self.input_frame = tkinter.Frame(master=tk, bg="#f5f5f5", bd=2)
         self.input_label = tkinter.Label(master=self.input_frame, text=label_text, justify=tkinter.CENTER, bd=2)
-        self.input_entry = tkinter.Entry(master=self.input_frame, justify=tkinter.RIGHT, bd=2)
+        self.input_entry = tkinter.Entry(master=self.input_frame, justify=tkinter.LEFT, bd=2)
         self.input_button = tkinter.Button(master=self.input_frame, width=5, text='...', command=self.open_key)
+        self.file_format = file_format
 
     def draw_interface(self, col):
         self.input_frame.grid(column=col, row=0, sticky="WESN")
@@ -18,7 +22,15 @@ class KeyInputBlock:
         self.input_frame.columnconfigure(1, weight=1)
 
     def open_key(self):
-        pass
+        f_path = filedialog.askopenfilename()
+        f_format = f_path.split('/')[-1].split('.')[-1]
+
+        if f_format == self.file_format:
+            self.file_path = f_path
+            self.input_entry.delete(0, tkinter.END)
+            self.input_entry.insert(0, self.file_path)
+        else:
+            messagebox.showinfo("Invalid file format", f"The file must have '.{self.file_format}' format")
 
 
 class TextBlock:
@@ -54,8 +66,8 @@ class Application(tkinter.Tk):
         self.title("Text encryption")
         self.iconbitmap('img/lock.ico')
 
-        self.public_key_block = KeyInputBlock(self, 'Public key')
-        self.private_key_block = KeyInputBlock(self, 'Private key')
+        self.public_key_block = KeyInputBlock(self, 'Public key', 'pub')
+        self.private_key_block = KeyInputBlock(self, 'Private key', 'prv')
         self.encrypt_text = TextBlock(self, 'Encrypt')
         self.decrypt_text = TextBlock(self, 'Decrypt')
 
