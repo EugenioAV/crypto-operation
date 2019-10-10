6b037f5d9936b42ce8d3cb3e0d7b735a8a99de7b
Eugenio <hedgehog0996@gmail.com>
2019-10-10 14:27:53 +0300

    functionality for encryption decryption of text

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter
+import rsa
 from tkinter import filedialog
 from tkinter import messagebox
 
@@ -10,7 +11,7 @@ class KeyInputBlock:
         self.input_frame = tkinter.Frame(master=tk, bg="#f5f5f5", bd=2)
         self.input_label = tkinter.Label(master=self.input_frame, text=label_text, justify=tkinter.CENTER, bd=2)
         self.input_entry = tkinter.Entry(master=self.input_frame, justify=tkinter.LEFT, bd=2)
-        self.input_button = tkinter.Button(master=self.input_frame, width=5, text='...', command=self.open_key)
+        self.input_button = tkinter.Button(master=self.input_frame, width=5, text='...', command=self.__get_key_path)
         self.file_format = file_format
 
     def draw_interface(self, col):
@@ -21,7 +22,7 @@ class KeyInputBlock:
 
         self.input_frame.columnconfigure(1, weight=1)
 
-    def open_key(self):
+    def __get_key_path(self):
         f_path = filedialog.askopenfilename()
         f_format = f_path.split('/')[-1].split('.')[-1]
 
@@ -30,14 +31,22 @@ class KeyInputBlock:
             self.input_entry.delete(0, tkinter.END)
             self.input_entry.insert(0, self.file_path)
         else:
-            messagebox.showinfo("Invalid file format", f"The file must have '.{self.file_format}' format")
+            messagebox.showinfo("Invalid file format", f"The file must has '.{self.file_format}' format")
+
+    def get_key(self):
+        key = None
+        if self.file_path:
+            with open(self.file_path, "rb") as file:
+                key = file.read()
+        return key
 
 
 class TextBlock:
-    def __init__(self, tk, button_text):
+
+    def __init__(self, tk, button_text, func):
         self.text_frame = tkinter.Frame(master=tk, bg="#f5f5f5", bd=2)
         self.input_text = tkinter.Text(master=self.text_frame)
-        self.button = tkinter.Button(master=self.text_frame, text=button_text, command=self.encrypt)
+        self.button = tkinter.Button(master=self.text_frame, text=button_text, command=func)
         self.output_text = tkinter.Text(master=self.text_frame)
 
     def draw_interface(self, col):
@@ -49,9 +58,6 @@ class TextBlock:
         self.text_frame.columnconfigure(0, weight=1)
         self.text_frame.rowconfigure(0, weight=1)
         self.text_frame.rowconfigure(2, weight=1)
-
-    def encrypt(self):
-        pass
 
 
 class Application(tkinter.Tk):
@@ -68,8 +74,8 @@ class Application(tkinter.Tk):
 
         self.public_key_block = KeyInputBlock(self, 'Public key', 'pub')
         self.private_key_block = KeyInputBlock(self, 'Private key', 'prv')
-        self.encrypt_text = TextBlock(self, 'Encrypt')
-        self.decrypt_text = TextBlock(self, 'Decrypt')
+        self.encrypt_text = TextBlock(self, 'Encrypt', self.__encrypt)
+        self.decrypt_text = TextBlock(self,  'Load data & Decrypt', self.__decrypt)
 
     def draw_interface(self):
         self.columnconfigure(0, weight=1)
@@ -80,6 +86,40 @@ class Application(tkinter.Tk):
         self.private_key_block.draw_interface(1)
         self.encrypt_text.draw_interface(0)
         self.decrypt_text.draw_interface(1)
+
+    @staticmethod
+    def save_data(data):
+        with open("output.txt", "wb") as file:
+            file.write(data)
+
+    def open_data(self):
+        with open("output.txt", "rb") as file:
+            data = file.read()
+        self.decrypt_text.input_text.insert(tkinter.END, data)
+
+        return data
+
+    def __encrypt(self):
+        self.encrypt_text.output_text.delete(1.0, tkinter.END)
+        file_key = self.public_key_block.get_key()
+        if file_key:
+            key = rsa.PublicKey.load_pkcs1(file_key)
+            message = self.encrypt_text.input_text.get(1.0, 'end-1c').encode('utf-8')
+            crypto = rsa.encrypt(message, key)
+            self.save_data(crypto)
+            self.encrypt_text.output_text.insert(tkinter.END, crypto)
+        else:
+            messagebox.showinfo("Error", "Input the path to the public key")
+
+    def __decrypt(self):
+        file_key = self.private_key_block.get_key()
+        if file_key:
+            self.decrypt_text.output_text.delete(1.0, tkinter.END)
+            self.decrypt_text.input_text.delete(1.0, tkinter.END)
+            key = rsa.PrivateKey.load_pkcs1(file_key)
+            self.decrypt_text.output_text.insert(tkinter.END, rsa.decrypt(self.open_data(), key))
+        else:
+            messagebox.showinfo("Error", "Input the path to the private key")
 
 
 if __name__ == '__main__':
